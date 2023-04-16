3847b999b73ea57d572e56295432e7b7a454aa4e
Jesica Viola <111540797+jesica-viola@users.noreply.github.com>
2023-04-16 20:54:48 -0300

    Actividad MundoPC
    Falta crear mas objetos (estan teclado, raton y monitor)

---
+++ MundoPC/src/mundopc/mundoPC.java
@@ -0,0 +1,29 @@
+package mundopc;
+
+import ar.com.system2023.mundopc.*; // se pone un * para q se importen todas las clases dentro de system2023
+
+
+public class mundoPC {
+    public static void main(String[] args) {
+        Monitor monitorHP = new Monitor("HP",13); // importar la clase
+        Teclado tecladoHP = new Teclado("Bluetooth", "HP");
+        Raton ratonHP = new Raton("Bluetooth", "HP");
+        Computadora computadoraHP = new Computadora("Computadora HP", monitorHP, tecladoHP, ratonHP);
+        
+        //creamos otros objetos de diferente marca
+        Monitor monitorGamer = new Monitor("Gamer",32); 
+        Teclado tecladoGamer = new Teclado("Bluetooth", "Gamer");
+        Raton ratonGamer = new Raton("Bluetooth", "Gamer");
+        Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+        Orden orden1 = new Orden();//inicializamos el arreglo vacio
+        Orden orden2 = new Orden();//una nueva lista para el objeto orden2
+        orden1.agregarComputadora(computadoraHP);
+        orden1.agregarComputadora(computadoraGamer);
+        
+        Computadora computadorasVarias = new Computadora("Computadora de diferentes marcas", monitorHP, tecladoGamer, ratonHP);
+        orden2.agregarComputadora(computadorasVarias);
+        
+        orden1.mostrarOrden();
+        orden2.mostrarOrden();
+    }
+}
