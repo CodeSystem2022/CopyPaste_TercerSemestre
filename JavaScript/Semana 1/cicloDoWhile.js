026914d5930bf7edbb4c055d939aab437b5ff5de
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-04-25 23:34:15 +0200

    1er Semana JavaScript

---
+++ JavaScript/Semana 1/cicloDoWhile.js
@@ -0,0 +1,10 @@
+/**** CICLO DO WHILE ****/
+// se ejecuta la menos una vez -> primero se ejecuta y luego ve si cumple la condición
+let conteo = 0
+
+do {
+  console.log(conteo)
+  conteo++ // operador unario de post incremento
+} while (conteo < 3);
+
+console.log("* Fin del ciclo Do While *")
---
+++ JavaScript/Semana 1/cicloWhile.js
@@ -0,0 +1,10 @@
+/* CICLO WHILE */
+// primero ve si cumple la condición y luego se ejecuta 
+let contador = 0
+
+while(contador < 3) {
+  console.log(contador)
+  contador++
+}
+
+console.log("* Fin del ciclo while *")
