794404916c0c1402239096de6afcb9069cc04c94
Jesica Viola <111540797+jesica-viola@users.noreply.github.com>
2023-05-12 21:29:38 -0300

    Python clase 5

--- Python/Leccion4/BD/prueba_bd.py
+++ Python/Leccion4/BD/prueba_bd.py
@@ -10,9 +10,10 @@ conexion = psycopg2.connect(
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = 'SELECT * FROM persona'
-            cursor.execute(sentencia) #de esta manera ejecutamos la sentencia
-            registros = cursor.fetchall()#recuperamos todos los registros que seran una lista
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s' # el asterisco indica todo el archivo pero se puede seleccionar una columna o fila o ID
+            id_persona= input('Digite un nro para el id_persona: ')
+            cursor.execute(sentencia, (id_persona,)) #de esta manera ejecutamos la sentencia
+            registros = cursor.fetchone()#recuperamos todos los registros que seran una lista si ponemos fetchone solo uno
             print(registros)
 
 except Exception as e:
