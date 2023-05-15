329055685d07d41589c223bdbfe36afca062541c
Jesica Viola <111540797+jesica-viola@users.noreply.github.com>
2023-05-15 00:20:02 -0300

    Python clase 5 parte 6

--- Python/Leccion4/BD/v_i_registros.py
+++ Python/Leccion4/BD/v_i_registros.py
@@ -10,8 +10,12 @@ try:
     with conexion:
         with conexion.cursor() as cursor:
             sentencia = 'INSERT INTO persona (nombre, apellido, email)VALUES (%s, %s, %s)'
-            valores = ('Carlos', 'Lara', 'clara@mail.com') #es una tupla
-            cursor.execute(sentencia, valores) #de esta manera ejecutamos la sentencia
+            valores = (
+                ('Carlos', 'Lara', 'clara@mail.com'),
+                ('Marcos', 'Canto', 'mcanto@mail.com'),
+                ('Marcelo', 'Cuenca', 'cuencam@mail.com')
+            ) #es una tupla de tuplas
+            cursor.executemany(sentencia, valores) #de esta manera ejecutamos la sentencia
             #conexion.commit() esto se utuliza para guardar los cambios en la base de datos
             registros_insertados = cursor.rowcount
             print(f'Los registros insertados son: {registros_insertados}')
