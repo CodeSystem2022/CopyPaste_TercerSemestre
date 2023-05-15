22dd4cbcf25d7877b09b49ead9c820096a91b476
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-05-15 12:48:45 -0400

    Semana 5 JavaScript objetos segunda parte

---
+++ JavaScript/Semana 5/objetos2.js
@@ -0,0 +1,78 @@
+let x = 10; //variable de tipo primitiva
+console.log(x.length);
+console.log('Tipos primitivos');
+//*Objeto*/
+let persona = {
+    nombre: 'Carlos',
+    apellido: 'Gil',
+    email: 'cgil@gmail.com',
+    edad: 28,
+    idioma: 'es',
+    get lang(){
+        return this.idioma.toUpperCase(); //Convierte las minúsculas a mayúsculas.
+    },
+    set lang(lang){
+        this.idioma = lang.toUpperCase();
+    },
+    nombreCompleto: function(){ // Método o funcion en JavaScript
+        return this.nombre+' '+this.apellido;
+    },
+    get nombreEdad(){
+        return 'El nombre es: '+this.nombre+' '+', Edad: '+this.edad;
+    }
+}
+
+console.log('Nombre: '+persona.nombre);
+console.log('Apellido: '+persona.apellido);
+console.log('Su email es: '+persona.email);
+console.log('De la edad de '+persona.edad +' años');
+console.log(persona.nombreCompleto());
+console.log('Ejecutando con un objeto');
+
+let persona2 = new Object();//Se debe crear un nuevo objeto en memoria
+persona2.nombre = "Juan";
+persona2.direccion = "Salada 14";
+persona2.telefono = "45134234512";
+console.log(persona2.telefono);
+console.log('Creamos un nuevo objeto');
+console.log(persona['apellido']); //Accedemos como si fuera un arreglo
+console.log('Usamos el ciclo for in');
+//For in y accedemos al objeto como si fuera un arreglo
+for (propiedad in persona) {
+    console.log(propiedad);
+    console.log(persona[propiedad]);
+    
+}
+console.log('Cambiamos y eliminamos un error');
+persona.apellida = 'Betancud'; //Cambiamos dinamicamente un valor del objeto
+delete persona.apellida; //Eliminamos el error
+console.log(persona);
+
+//* DISTINTAS FORMAS DE IMPRIMIR UN OBJETO */ 
+
+//Número 1 (la más sencilla): concatenar cada valor de cada propiedad.
+console.log('Distintas formas de imprimir un objeto: forma 1');
+console.log(persona.nombre+', '+persona.apellido);
+
+//Número 2: a través del ciclo "for in"
+console.log('Distintas formas de imprimir un objeto: forma 2');
+for(nombrePropiedad in persona){
+    console.log(persona[nombrePropiedad]);
+}
+
+//Número 3: la función object.values()
+console.log('Distintas formas de imprimir un objeto: forma 3');
+let personaArray = Object.values(persona);
+console.log(personaArray);
+
+// Número 4: Utilizaremos el método JSON.stringify
+console.log('Distintas formas de imprimir un objeto: forma 4');
+let personaString = JSON.stringify(persona);
+console.log(personaString);
+
+console.log('Comenzamos a utilizar el metodo get')
+console.log(persona.nombreEdad);
+
+console.log('Comenzamos con el método get y set para idiomas');
+persona.lang = 'en';
+console.log(persona.lang);
