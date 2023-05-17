e719341930aec986c4cf33c85d27d9d9e4cd159c
Jesica Viola <111540797+jesica-viola@users.noreply.github.com>
2023-05-17 02:00:08 -0300

    Clase 3 parte 4

--- Java/Leccion03/AutoboxingUnboxing/src/test/TestAutoboxinUnboxing.java
+++ Java/Leccion03/AutoboxingUnboxing/src/test/TestAutoboxinUnboxing.java
@@ -7,10 +7,18 @@ public class TestAutoboxinUnboxing {
         // clases envolventes o wrapper
         //Clases envolventes de tipos primitivos:
         //int= la clase envolvente es Integer
+        //long= la clase envolvente es Long
+        //float= la clase envolver es Float
+        //doluble= la clase envolver es Doluble
+        //boolean = la clase envolver es Byte
+        //char= la clase envolvente es Character
+        //short= la clase envolvente es Short
         int enteroPrim = 10; // tipo primitivo
         System.out.println("enteroPrim = " + enteroPrim);
         Integer entero =10;// tipo objetc con la clase Integer
-        System.out.println("entero = " + entero.toString());
+        System.out.println("entero = " + entero.doubleValue()); //autoboxing: convertir el objeto a un tipo primitivo y viceversa
         
+        int entero2= entero; // unboxing: ya no es un objeto es un tipo primitivo
+        System.out.println("entero2 = " + entero2);
     }
 }
