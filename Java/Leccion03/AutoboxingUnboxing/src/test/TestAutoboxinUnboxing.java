a36f50cae4833a28e4517f136cf8b44edb309a47
Jesica Viola <111540797+jesica-viola@users.noreply.github.com>
2023-05-17 01:49:43 -0300

    Java clase 3 parte 3

---
+++ Java/Leccion03/AutoboxingUnboxing/src/test/TestAutoboxinUnboxing.java
@@ -0,0 +1,16 @@
+
+package test;
+
+
+public class TestAutoboxinUnboxing {
+    public static void main(String[] args) {
+        // clases envolventes o wrapper
+        //Clases envolventes de tipos primitivos:
+        //int= la clase envolvente es Integer
+        int enteroPrim = 10; // tipo primitivo
+        System.out.println("enteroPrim = " + enteroPrim);
+        Integer entero =10;// tipo objetc con la clase Integer
+        System.out.println("entero = " + entero.toString());
+        
+    }
+}
