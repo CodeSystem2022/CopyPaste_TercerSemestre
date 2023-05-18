5f31395aaa5625d88364397bce26beac6f32eea7
Jesica Viola <111540797+jesica-viola@users.noreply.github.com>
2023-05-17 21:50:59 -0300

    Java clase 3 parte 7

--- Java/Leccion03/ModificadoresAcceso/src/test/TestModificadoresAcceso.java
+++ Java/Leccion03/ModificadoresAcceso/src/test/TestModificadoresAcceso.java
@@ -6,5 +6,8 @@ public class TestModificadoresAcceso {
 
     public static void main(String[] args) {
         
+        //amiga aca arriba de esto, yo despues lo arreglo
+         //clase3 claseHija=new Clase3();
+         //System.out.println("claseHija = " + claseHija);
     }
 }
