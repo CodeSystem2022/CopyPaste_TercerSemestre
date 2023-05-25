7778415c8784f13862fb3c45e3455ff3594dba87
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-05-25 14:11:43 -0400

    Transacciones 3 conexión a base de datos

---
+++ Python/Leccion4/transacciones_tres.py
@@ -0,0 +1,7 @@
+import psycopg2 as bd #esto es para poder conectarnos a Postgres
+conexion = bd.connect(
+    user='postgres',
+    password='4271369.',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd')
