ad2020cca9bc401e9116d04a59c2a5a56f25451e
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-06-04 12:20:56 +0200

    Conexion a la DB de la clase 7 de python

---
+++ Python/Clase 7/capa_datos_persona/conxion.py
@@ -0,0 +1,8 @@
+class Conexion:
+    _DATABASE = 'test_bd'
+    _USERNAME = 'postgres'
+    _PASSWORD = '4271369.'
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _conexion = None
+    _cursor = None
