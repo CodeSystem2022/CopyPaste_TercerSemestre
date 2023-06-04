0320ae01c4a23fc89333a0c995999a6681903f3f
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-06-04 21:42:45 +0200

    parte 5 de la clase 8 de Python

--- Python/Clase 7/capa_datos_persona/conxion.py
+++ Python/Clase 7/capa_datos_persona/conxion.py
@@ -29,3 +29,19 @@ class Conexion:
             else:
                 return cls._conexion
                 
+@classmethod
+    def obtenerCursor(cls):
+        if cls._cursor is None:
+            try:
+                cls._cursor= cls.obtenerConexion().cursor()
+                log.debug(f'Se abrio correctamente el cursor: {cls._cursor}')
+                return cls._cursor
+            except Exception as e:
+                log.error(f'Ocurrio un error:{e}')
+                sys.exit()
+            else:
+                return cls._cursor
+if _name=='main_':
+    Conexion.obtenerConexion()
+    Conexion.obtenerCursor()
+    
