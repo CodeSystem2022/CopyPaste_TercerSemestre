ff3464110a9883e04ec41dfb580021c841db807c
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-06-04 21:37:25 +0200

    classmethod de conexion Clase 7 de Python

--- Python/Clase 7/capa_datos_persona/conxion.py
+++ Python/Clase 7/capa_datos_persona/conxion.py
@@ -12,3 +12,20 @@ class Conexion:
     _conexion = None
     _cursor = None
 
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion= bd.connect(host=cls._HOST,
+                                          user=cls._USERNAME,
+                                          password=cls._PASSWORD,
+                                          port=cls._DB_PORT,
+                                          database=cls._DATABASE)
+                log.debug(f'Conexion Exitosa: {cls._conexion}')
+                return cls._conexion
+            except Exception as e:
+                log.error(f'Ocurrio un error: {e}')
+                sys.exit()
+            else:
+                return cls._conexion
+                
