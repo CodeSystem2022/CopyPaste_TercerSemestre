9f0cd8eb3563985e69f3370e7d2f2ed9cd2a1a6a
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-06-15 22:10:23 +0200

    Herencia Semana 9

--- JavaScript/Semana 9/PruebaPersonas.js
+++ JavaScript/Semana 9/PruebaPersonas.js
@@ -1,3 +1,5 @@
+//** Herencia */
+
 class Persona{
 
     static contadorPersonas = 0;
@@ -84,19 +86,19 @@ class Cliente extends Persona{
     
 }
 
-//Prueba clase Persona
+//**Prueba clase Persona
 let persona1 = new Persona('Juan', 'Perez',32);
 console.log(persona1.toString());
 let persona2 = new Persona('Carla', 'Ortega',22);
 console.log(persona2.toString());
 
-//Prueba clase Empleado
+//**Prueba clase Empleado
 let empleado1 = new Empleado('Pedro', 'Roman', 18, 5000);
 console.log(empleado1.toString());
 let empleado2 = new Empleado('Jonas', 'Torres', 30, 7000);
 console.log(empleado2.toString());
 
-//Prueba clase Cliente
+//**Prueba clase Cliente
 let cliente1 = new Cliente('Miguel', 'Zala', 29, new Date());
 console.log(cliente1.toString());
 let cliente2 = new Cliente('Natalia', 'Ortega', 22, new Date());
