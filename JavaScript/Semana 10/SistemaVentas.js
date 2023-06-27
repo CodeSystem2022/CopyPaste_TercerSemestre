f8de393c4b855b514eef466716499e861425be8d
Leo <tengopaye@gmail.com>
2023-06-27 00:27:08 -0300

    JavaScript Clase 10 class Orden + instanciación de clases

--- JavaScript/Semana 10/SistemaVentas.js
+++ JavaScript/Semana 10/SistemaVentas.js
@@ -32,9 +32,63 @@ class Producto{
         }
     } //Fin de la clase producto
 
-//!Código Jesi
-//** Clase Orden */
-//Fin de la clase orden
 
-//!Código Leo
+//** Clase Orden */
+class Orden{
+    static contadorOrdenes = 0;
+    static getMAX_PRODUCTOS(){ //Simula una constante
+    return 5;
+    }
+    
+    constructor(){
+    this._idOrden = ++Orden.contadorOrdenes;
+    this._productos = [];
+    this._contadorProductosAgregados = 0;
+    }
+    
+    get _idOrden(){
+    return this._idOrden;
+    }
+    
+    agregarProducto(producto){
+    if(this._productos.length < Orden.getMAX_PRODUCTOS()){
+    this._productos.push(producto); //Tenemos dos tipos de sintaxis: 1
+    //this._productos[this._contadorProductosAgregados++] = producto; //segunda sintaxis
+    }
+    else{
+    console.log('No se pueden agregar mas productos')
+    }
+    } // Fin del metodo agregarProducto
+    
+    clacularTotal(){
+    let totalVenta = 0;
+    for(let producto of this._productos){
+    totalVenta += producto.precio;
+    }//fin ciclo for
+    return totalVenta;
+    }// fin metodo calcular total
+    
+    mostrarOrden(){
+    let productosOrden = ' ';
+    for(let producto of this._productos){
+    productosOrden += '\n{'+producto.toString()+' }';
+    } //fin del ciclo For
+    console.log(`Orden: ${this._idOrden}, Total:$${this.clacularTotal()}, Productos: ${productosOrden}`);
+    }//Fin metodo mostrarOrden
+    }//Fin de la clase Orden     
+
+
 //** Instancias de las clases */
+let producto1 = new Producto('Pantalon', 200);
+let producto2 = new Producto('Camisa', 150);
+let producto3 = new Producto('Cinturón', 50);
+let orden1 = new Orden;
+let orden2 = new Orden();
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+orden1.agregarProducto(producto3);
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+orden2.agregarProducto(producto3);
+orden1.mostrarOrden();
+orden2.mostrarOrden();
