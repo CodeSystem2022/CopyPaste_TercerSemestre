1ab552d84d6eb5d6706d3ea647700345f1ace596
Jesica Viola <111540797+jesica-viola@users.noreply.github.com>
2023-06-27 20:29:13 -0300

    Java clase 11 Conexion parte 2

--- Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Main.java
+++ Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Main.java
@@ -1,7 +1,13 @@
 package UTN;
+import UTN.conexion.Conexion;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        var conexion = Conexion.getConnection();
+        if(conexion != null) {
+            System.out.println("Conexión exitosa: "+conexion);
+        } else {
+            System.out.println("Error al conectarse");
+        }
+    }//Fin main
+}//Fin clase
