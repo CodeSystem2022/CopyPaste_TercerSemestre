5fc734188253acb5a1bf2845ba64d7afc8da0983
Leila Britez Neira <britezneira.leila@gmail.com>
2023-06-30 13:33:25 +0200

    Java clase 12 clase EstudianteDAO
    Método listarEstudiantes()

---
+++ Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -0,0 +1,48 @@
+package UTN.datos;
+
+import UTN.dominio.Estudiante;
+
+import static UTN.conexion.Conexion.getConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstudianteDAO {
+//Método listar
+  public List<Estudiante> listarEstudiantes() {
+    List<Estudiante> estudiantes = new ArrayList<>();
+        //Creamos algunos objetos que son necesarios para comunicarnos con la base de datos
+    PreparedStatement ps; //Envía la sentencia a la base de datos
+    ResultSet rs; //Obtenemos el resultao de la base de datos
+        //Creamos un objeto de tipo conexión
+    Connection con = getConnection();
+    String sql = "SELECT * FROM estudiantes ORDER BY idestudiantes2022";
+    try {
+      ps = con.prepareStatement(sql);
+      rs = ps.executeQuery();
+      while(rs.next()) {
+        var estudiante = new Estudiante();
+        estudiante.setIdEstudiante(rs.getInt("idestudiantes2022"));
+        estudiante.setNombre(rs.getString("nombre"));
+        estudiante.setApellido(rs.getString("apellido"));
+        estudiante.setTelefono(rs.getString("telefono"));
+        estudiante.setEmail(rs.getString("email"));
+          //Falta agregarlo a la lista
+        estudiantes.add(estudiante);
+      }
+    
+    } catch (Exception e) {
+      System.out.println("Ocurrió un error al seleccionar datos: "+e.getMessage());
+      }
+    finally {
+      try {
+        con.close();
+      } catch (Exception e) {
+        System.out.println("Ocurrió un error al cerrar la conexión: "+e.getMessage());
+      }
+     }//Fin finally
+    return estudiantes;
+  }//Fin método listar
+
