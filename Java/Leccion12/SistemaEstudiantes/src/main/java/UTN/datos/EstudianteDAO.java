07b15445b68d8131da92e9d682b1bc1d3a7b4efc
Leila Britez Neira <britezneira.leila@gmail.com>
2023-06-30 13:44:22 +0200

    Java clase 12 clase EstudianteDAO
    Método modificarEstudiante()

--- Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -74,7 +74,7 @@ public class EstudianteDAO {
       }//Fin cacth
     }//Fin finally
     return false;
-  }
+  } //Fin del método buscarEstudiantePorId
 
   //Método agregar un nuevo estudiante
   public boolean agregarEstudiante(Estudiante estudiante) {
@@ -101,4 +101,32 @@ public class EstudianteDAO {
     }//Fin finally
     return false;
   }//Fin método agregarEstudiante
+
+  //Método para modificar estudiante
+  public boolean modificarEstudiante(Estudiante estudiante){
+    PreparedStatement ps;
+    Connection con = getConnection();
+    String sql = "UPDATE estudiantes2022 SET nombre=?, apellido=?, telefono=?, email=? WHERE idestudiantes2022=?";
+    try {
+      ps = con.prepareStatement(sql);
+      ps.setString(1, estudiante.getNombre());
+      ps.setString(2, estudiante.getApellido());
+      ps.setString(3, estudiante.getTelefono());
+      ps.setString(4, estudiante.getEmail());
+      ps.setInt(5, estudiante.getIdEstudiante());
+      ps.execute();
+      return true;
+    } catch (Exception e) {
+      System.out.println("Error al modificar estudiante: "+e.getMessage());
+    }//Fin catch
+    finally {
+      try {
+        con.close();
+      } catch (Exception e) {
+        System.out.println("Error al cerrar la conexión: "+e.getMessage());
+      }//Fin catch
+    }//Fin finally
+    return false;
+  }//Fin método modificarEstudiante
+  
   
