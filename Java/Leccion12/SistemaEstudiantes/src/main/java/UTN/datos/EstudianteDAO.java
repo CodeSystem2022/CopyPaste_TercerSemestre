0945dc05a62caf468ed67b1cb5a92910634ab471
Leila Britez Neira <britezneira.leila@gmail.com>
2023-06-30 13:35:58 +0200

    Java clase 12 clase EstudianteDAO
    método buscarEstudiantePorId()

--- Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -46,3 +46,33 @@ public class EstudianteDAO {
     return estudiantes;
   }//Fin método listar
 
+//Método por id -> fin by id
+  public boolean buscarEstudiantePorId(Estudiante estudiante) {
+    PreparedStatement ps;
+    ResultSet rs;
+    Connection con = getConnection();
+    String sql = "SELECT + FROM estudiantes2022 WHERE idestudiantes2022=?";
+    try {
+      ps = con.prepareStatement(sql);
+      ps.setInt(1, estudiante.getIdEstudiante());
+      rs = ps.executeQuery();
+      if(rs.next()) {
+        estudiante.setNombre(rs.getString("nombre"));
+        estudiante.setApellido(rs.getString("apellido"));
+        estudiante.setTelefono(rs.getString("telefono"));
+        estudiante.setEmail(rs.getString("email"));
+        return true;  // Se encontró un registro
+      }//Fin if
+    } catch (Exception e) {
+      System.out.println("Ocurrió un error al buscar estudiante: "+e.getMessage());
+    }
+    finally {
+      try {
+        con.close();
+      } catch (Exception e) {
+        System.out.println("Ocurrió un error al cerrar la conexión: "+e.getMessage());
+      }//Fin cacth
+    }//Fin finally
+    return false;
+  }
+  
