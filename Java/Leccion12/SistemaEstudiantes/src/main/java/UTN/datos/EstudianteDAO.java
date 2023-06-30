6e0e090bf99e889b83cf5fae09d4c2d439a1b927
Leila Britez Neira <britezneira.leila@gmail.com>
2023-06-30 13:37:42 +0200

    Java clase 12 clase EstudianteDAO
    Método agregarEstudiante()

--- Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -75,4 +75,30 @@ public class EstudianteDAO {
     }//Fin finally
     return false;
   }
+
+  //Método agregar un nuevo estudiante
+  public boolean agregarEstudiante(Estudiante estudiante) {
+    PreparedStatement ps;
+    Connection con = getConnection();
+    String sql = "INSERT INTO estudiantes2022 (nombre, apeLlido, telefono, email) VALUES (?,?,?,?)";
+    try {
+      ps = con.prepareStatement(sql);
+      ps.setString(1, estudiante.getNombre());
+      ps.setString(2, estudiante.getApellido());
+      ps.setString(3, estudiante.getTelefono());
+      ps.setString(4, estudiante.getEmail());
+      ps.execute();
+      return true;
+    } catch (Exception e) {
+      System.out.println("Ocurrió un error al agregar estudiante: "+e.getMessage());
+    }//Fin catch
+    finally {
+      try {
+        con.close();
+      } catch (Exception e) {
+        System.out.println("Error al cerrar la conexión: "+e.getMessage());
+      }//Fin cacth
+    }//Fin finally
+    return false;
+  }//Fin método agregarEstudiante
   
